a90cac7b9aa86608746d829455fb8c69f7b41c42
TakoFrog <canhlong2012@gmail.com>
2024-01-06 15:38:27 +0700

    more CSS less Goofy Ahh

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+app.use(express.static(__dirname));
 io.on("connection",(socket)=>{
 console.log("new user connected");
 
